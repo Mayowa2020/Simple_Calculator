b952c7977a5cf6aed0efdea0213262bf25e71c65
Mayowa2020 <73591664+Mayowa2020@users.noreply.github.com>
2022-06-23 21:21:52 +0100

    Created a Simple Calculator using JavaScript

---
+++ calculator.js
@@ -0,0 +1,17 @@
+let num1 = 0;
+let num2 = 0;
+let option = "";
+
+option = prompt(`Pick operation: +, -, *, /`);
+num1 = parseInt(prompt("Enter first number"));
+num2 = parseInt(prompt("Enter second number"));
+
+if (option == "+") {
+	alert(num1 + num2);
+} else if (option == "-") {
+	alert(num1 - num2);
+} else if (option == "*") {
+	alert(num1 * num2);
+} else (option == "/") {
+	alert(num1 / num2);
+}
